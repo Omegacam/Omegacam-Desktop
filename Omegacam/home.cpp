ee87d1df750e8225f60e181d008677f7baa33a70
richy6 <richardwei6@gmail.com>
2020-11-27 21:53:55 -0800

    Adjust scrollbar style

--- Omegacam/home.cpp
+++ Omegacam/home.cpp
@@ -18,6 +18,32 @@ home::home(QWidget *parent):QMainWindow(parent), ui(new Ui::home){
     rightScrollArea->setWidget(cameraList);
     rightScrollArea->setWidgetResizable(true);
     rightScrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    rightScrollArea->setStyleSheet(QString::fromUtf8("QScrollBar:vertical {"
+    "    border: 1px solid #999999;"
+    "    background:white;"
+    "    width:10px;    "
+    "    margin: 0px 0px 0px 0px;"
+    "}"
+    "QScrollBar::handle:vertical {"
+    "    background: qlineargradient(x1:0, y1:0, x2:1, y2:0,"
+    "    stop: 0 rgb(66, 66, 66), stop: 0.5 rgb(66, 66, 66), stop:1 rgb(97, 97, 97));"
+    "    min-height: 0px;"
+    "}"
+    "QScrollBar::add-line:vertical {"
+    "    background: qlineargradient(x1:0, y1:0, x2:1, y2:0,"
+    "    stop: 0 rgb(66, 66, 66), stop: 0.5 rgb(66, 66, 66),  stop:1 rgb(97, 97, 97));"
+    "    height: 0px;"
+    "    subcontrol-position: bottom;"
+    "    subcontrol-origin: margin;"
+    "}"
+    "QScrollBar::sub-line:vertical {"
+    "    background: qlineargradient(x1:0, y1:0, x2:1, y2:0,"
+    "    stop: 0  rgb(66, 66, 66), stop: 0.5 rgb(66, 66, 66),  stop:1 rgb(97, 97, 97));"
+    "    height: 0 px;"
+    "    subcontrol-position: top;"
+    "    subcontrol-origin: margin;"
+    "}"
+    ));
     //rightScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     //rightScrollArea->show();
 }
